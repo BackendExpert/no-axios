652d21fc69f3d84b6bf705871864afcbaecd102c
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-17 19:39:29 +0530

    Update index.js
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+anupa1998@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 import axios from "axios";
 require('dotenv').config();
 
-console.log(process.env.REACT_SERVER)
+// console.log(process.env.REACT_SERVER)
+
+function SendToBackEnd(Server, apiPath, data){
+    axios.post(`${Server} ${apiPath}`)
+    console.log(Server, apiPath)
+}
+
+module.exports = {
+    SendToBackEnd,
+}
